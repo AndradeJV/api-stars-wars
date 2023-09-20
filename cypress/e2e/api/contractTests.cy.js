cab5b9235c595ef9238030024809f501aefe6aa5
Joao Andrade <joaovandrade30@gmail.com>
2023-09-20 13:05:33 -0300

    My changes

--- cypress/e2e/api/contractTests.cy.js
+++ cypress/e2e/api/contractTests.cy.js
@@ -4,6 +4,7 @@ import { requestInvalido } from '../../fixtures/invalid_Url'
 describe('Valida Response', () => {
 
     let mensagem = 'detail:Not found'
+    let fixtureTitles = ['A New Hope', 'The Empire Strikes Back', 'Return of the Jedi', 'The Phantom Menace', 'Attack of the Clones', 'Revenge of the Sith']
 
     context('Valida Request Válido', () => {
 
@@ -13,15 +14,20 @@ describe('Valida Response', () => {
                 method: 'GET',
                 url: 'films/?format=json'
             }).then(({ status, body }) => {
-                expect(status).to.eq(200)
-                expect(body.count).eq(6)
-                expect(body.results[0].episode_id).eq(4)
-                expect(body.results[0].title).eq('A New Hope')
-                expect(body.results[1].title).eq('The Empire Strikes Back')
-                expect(body.results[2].title).eq('Return of the Jedi')
-                expect(body.results[3].title).eq('The Phantom Menace')
-                expect(body.results[4].title).eq('Attack of the Clones')
-                expect(body.results[5].title).eq('Revenge of the Sith')
+                // expect(status).to.eq(200)
+                // expect(body.count).eq(6)
+                // expect(body.results[0].episode_id).eq(4)
+                // expect(body.results[0].title).eq('A New Hope')
+                // expect(body.results[1].title).eq('The Empire Strikes Back')
+                // expect(body.results[2].title).eq('Return of the Jedi')
+                // expect(body.results[3].title).eq('The Phantom Menace')
+                // expect(body.results[4].title).eq('Attack of the Clones')
+                // expect(body.results[5].title).eq('Revenge of the Sith')
+
+
+                for(let i = 0; i < fixtureTitles.length; i++){
+                    expect(body.results[i].title).to.eq(fixtureTitles[i])
+                }
 
                 cy.testeContrato(testeContratoFilms, body)
             })
@@ -32,7 +38,7 @@ describe('Valida Response', () => {
 
     context('Valida Request Inválido', () => {
 
-        it.only('Request Inválido', () => {
+        it('Request Inválido', () => {
             cy.request({
                 method: 'GET',
                 url: 'people/?format=jsonx',
